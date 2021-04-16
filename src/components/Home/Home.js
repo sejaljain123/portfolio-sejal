b93d9870089ce8bc53000b9dbdbd9ddd10b77c60
sejaljain123 <sejaljain2043@gmail.com>
2021-04-16 14:10:50 +0530

    portfolio

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className = "profile">
+            <h1>HI, I am Sejal Jain!</h1>
+        </div>
+    )
+}
+
+export default Home
+
