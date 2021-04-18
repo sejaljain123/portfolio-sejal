7cd1c2a3d81dde1338ed7947f0a0dbac12c44027
sejaljain123 <sejaljain2043@gmail.com>
2021-04-18 23:40:12 +0530

    change footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,19 +17,19 @@ function Footer() {
           href="https://www.facebook.com/profile.php?id=100039726875356"
           className="facebook social"
         >
-          <FontAwesomeIcon icon={faFacebook} size="2x" />
+          <FontAwesomeIcon icon={faFacebook} size="2x" fill="#ffffff" />
         </a>
         <a href="https://www.instagram.com/sejaljain2001" className="instagram social">
-          <FontAwesomeIcon icon={faInstagram} size="2x" />
+          <FontAwesomeIcon icon={faInstagram} size="2x" fill="#ffffff" />
         </a>
         <a href="https://twitter.com/SejalJa68705849?s=08" className="twitter social ">
-          <FontAwesomeIcon icon={faTwitter} size="2x" />
+          <FontAwesomeIcon icon={faTwitter} size="2x" fill="#ffffff" />
         </a>
         <a href="https://github.com/sejaljain123" className="github social">
-          <FontAwesomeIcon icon={faGithub} size="2x" />
+          <FontAwesomeIcon icon={faGithub} size="2x" fill="#ffffff" />
         </a>
         <a href="https://www.linkedin.com/in/sejal-jain-5a7536194" className="linkedin social">
-          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          <FontAwesomeIcon icon={faLinkedin} size="2x" fill="#ffffff" />
         </a>
       </div>
     </div>
