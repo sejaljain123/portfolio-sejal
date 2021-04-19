4958529397e0d98ced9bcd40b2b3b93e941c85c6
sejaljain123 <sejaljain2043@gmail.com>
2021-04-19 22:14:22 +0530

    change theme

--- src/components/Theme/Theme.js
+++ src/components/Theme/Theme.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import './Theme.css';
+const themes = [
+  {
+    background: '#fef6e4',
+    buttoncolor: '#f582ae',
+    buttontext: '#001858',
+    textcolor: '#001858',
+  },
+  {
+    background: '#0f0e17',
+    buttoncolor: '#ff8906',
+    buttontext: '#fffffe',
+    textcolor: '#fffffe',
+  },
+];
 function Theme() {
-  const themes = [
-    {
-      background: '#fef6e4',
-      buttoncolor: '#f582ae',
-      buttontext: '#001858',
-      textcolor: '#001858',
-    },
-    { background: '#0f0e17', buttoncolor: '#ff8906', buttontext: '#fffffe', textcolor: '#fffffe' },
-  ];
   const [index, setIndex] = useState(0);
   const changeTheme = (index) => {
     const { background, textcolor, buttoncolor, buttontext } = themes[index];
